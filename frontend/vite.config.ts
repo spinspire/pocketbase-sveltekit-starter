619ca37220b7a9884e36972215a96b0adaffb876
Jitesh Doshi <jitesh@spinspire.com>
2022-09-15 22:08:40 -0400

    proxy backend via vite server to avoid CORS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8090',
+    }
+  }
 };
 
 export default config;
