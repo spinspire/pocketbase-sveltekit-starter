6b3a335902cc3a8215a3547a97fec53bcc4556ad
Jitesh Doshi <jitesh@spinspire.com>
2022-09-16 20:43:23 -0400

    add Image component for better IMG handling

---
+++ frontend/src/lib/components/Image.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { client } from "$lib/pocketbase";
+  import type { Record } from "pocketbase";
+
+  export let record: Record;
+  export let file: string;
+  export let thumb: string | undefined;
+
+  let props: any;
+  $: ({ record, file, thumb, ...props } = $$props);
+  $: src = file
+    ? client.records.getFileUrl(record, file, { thumb })
+    : `https://via.placeholder.com/${thumb ?? "100x100"}`;
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<img {...props} {src} rel="noreferrer" />
