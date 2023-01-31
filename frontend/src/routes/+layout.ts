097c16b49c1c5a313e72730803b380dfc0def0e4
Shafik Amin <shafik23@gmail.com>
2023-01-30 23:49:45 -0700

    Disabled SSR globally until I can understand it better!

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,5 +1,5 @@
 // turning off SSR for now, but should look at this again
-// export const ssr = false;
+export const ssr = false;
 
 // Prerendering turned off. Turn it on if you know what you're doing.
 // export const prerender = true;
