1f922ade56cdff1169f67d61c79b0b4d53e863b6
Shafik Amin <shafik23@gmail.com>
2023-02-01 17:34:40 -0700

    Modified users table rules

---
+++ backend/pb_migrations/1675298008_updated_users.js
@@ -0,0 +1,33 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": true,
+    "allowUsernameAuth": false,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "requireEmail": false
+  }
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": true,
+    "allowUsernameAuth": true,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "requireEmail": false
+  }
+
+  return dao.saveCollection(collection)
+})
