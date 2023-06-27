d0cec9b84a04463d7e72eaae1e5a4fc5f1aa12e9
Austin Woods <austinwoods99@gmail.com>
2023-06-27 14:04:56 -0400

    add components: TabGroup, Tab, TabContent

---
+++ sk/src/lib/components/TabContent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let key: string | number;
+  import { getContext } from "svelte";
+  import type { Writable } from "svelte/store";
+  const store: Writable<string | number> = getContext("activeTab");
+</script>
+
+{#if $store === key}
+  <slot />
+{/if}
