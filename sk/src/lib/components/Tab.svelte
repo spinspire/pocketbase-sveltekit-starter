a0cc660f3895ff352e3552a6eaf8a8a5b9690749
Austin Woods <austinwoods99@gmail.com>
2023-06-27 14:04:56 -0400

    add components: TabGroup, Tab, TabContent

---
+++ sk/src/lib/components/Tab.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let key: string | number;
+  import { getContext } from "svelte";
+  import type { Writable } from "svelte/store";
+  const store: Writable<string | number> = getContext("activeTab");
+</script>
+
+<div
+  class="title"
+  on:click={() => ($store = key)}
+  class:active={$store === key}
+>
+  <slot />
+</div>
+
+<style>
+  .title {
+    /* border: solid lightgray 2px; */
+    border-bottom: none;
+    display: inline-block;
+    padding: 1em;
+    color: var(--tab-color-fg, white);
+    background-color: var(--tab-color-bg, black);
+  }
+  .title.active {
+    color: var(--tab-color-active-fg, black);
+    background-color: var(--tab-color-active-bg, white);
+  }
+  .title:not(.active):hover {
+    cursor: pointer;
+    color: var(--tab-color-hover-fg, black);
+    background-color: var(--tab-color-hover-bg, lightgray);
+  }
+</style>
---
+++ sk/src/lib/components/TabGroup.svelte
@@ -0,0 +1,25 @@
+<script type="ts">
+  export let active: string | number = 0;
+  import { writable } from "svelte/store";
+  import type { Writable } from "svelte/store";
+  import { setContext } from "svelte";
+  const store: Writable<string | number> = writable(active);
+  setContext("activeTab", store);
+</script>
+
+<style>
+  .tabs {
+    border-bottom: solid var(--tab-color-active-bg, black) 2px;
+  }
+  .tab-content {
+    padding: 1em;
+  }
+</style>
+
+<div class="tabs">
+  <slot name="tabs" />
+</div>
+
+<div class="tab-content">
+  <slot />
+</div>
