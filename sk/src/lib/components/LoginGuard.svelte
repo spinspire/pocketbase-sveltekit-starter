7ae9f6b68e85c1fbcee1830f85648674c81ea708
Jitesh Doshi <jitesh@spinspire.com>
2023-07-24 13:49:51 -0400

    LoginGuard: add "destination" and "admin" props

--- sk/src/lib/components/LoginGuard.svelte
+++ sk/src/lib/components/LoginGuard.svelte
@@ -1,10 +1,22 @@
-<script>
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Admin } from "pocketbase";
   import { authModel } from "../pocketbase";
   import LoginForm from "./LoginForm.svelte";
+  export let admin: boolean | undefined = undefined;
   export let slotLogin = false;
+  export let destination: string | null = null;
+  $: if (destination != null && $authModel) {
+    goto(destination);
+  }
+  $: authorized =
+    $authModel && //  must be logged in
+    (admin === undefined || // admin or not, doesn't matter
+      (admin === true && $authModel instanceof Admin) || // must be admin
+      (admin === false && !($authModel instanceof Admin))); // must not be admin
 </script>
 
-{#if $authModel}
+{#if authorized}
   <slot />
 {:else if slotLogin || $$slots["login"]}
   <slot name="login">
