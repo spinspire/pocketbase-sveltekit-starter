10b2b0a569bf2559fc59142d9667329f671d6bb4
Jitesh Doshi <jitesh@spinspire.com>
2024-05-28 18:24:19 -0400

    add message to browse posts

--- sk/src/routes/+page.svelte
+++ sk/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { base } from "$app/paths";
+
   const { data } = $props();
   $effect(() => {
     // you could set the metadata either here or in +page.ts
@@ -28,3 +30,5 @@
   <li>PocketBase: CRUD, typegen, realtime data updates</li>
   <li>PocketBase: JSVM hook, routes, etc.</li>
 </ul>
+
+<p>Now <a href="{base}/posts">browse/edit/create some posts</a>.</p>
