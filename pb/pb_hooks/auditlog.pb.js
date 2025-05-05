275490248df865a2a8393e94311cda563ca4bc85
Will <relkenwilliam@gmail.com>
2025-05-05 03:42:37 +0000

    fix bug in js auditlog

--- pb/pb_hooks/auditlog.pb.js
+++ pb/pb_hooks/auditlog.pb.js
@@ -3,16 +3,19 @@
 // auditlog generation
 onRecordCreateRequest((e) => {
   const { doAudit } = require(`${__hooks}/util`);
+  e.next();
   doAudit("insert", e);
   return e.next();
 });
 onRecordUpdateRequest((e) => {
   const { doAudit } = require(`${__hooks}/util`);
+  e.next();
   doAudit("update", e);
   return e.next();
 });
 onRecordDeleteRequest((e) => {
   const { doAudit } = require(`${__hooks}/util`);
+  e.next();
   doAudit("delete", e);
   return e.next();
 });
