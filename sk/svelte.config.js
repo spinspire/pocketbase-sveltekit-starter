ba4e3e865a47708576e9caea8ea4d4d94f0e2a16
Jitesh Doshi <jkdoshi@users.noreply.github.com>
2026-08-22 14:53:21 -0400

    feat: integrate mdsvex for markdown pages
    - Install mdsvex@0.12.8
    - Configure svelte.config.js preprocessor for .md files

--- sk/svelte.config.js
+++ sk/svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: [
+    preprocess(),
+    mdsvex({
+      extensions: [".md"],
+    }),
+  ],
 
   kit: {
     alias: {
